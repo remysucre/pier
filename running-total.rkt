079ce102592ddff1f617230a8ab8ce3fb97dd64a
Remy Wang <remy.sucre@gmail.com>
2020-10-19 19:51:11 -0700

    Running total passes.

--- running-total.rkt
+++ running-total.rkt
@@ -1,32 +1,16 @@
 #lang rosette
 
-(define N 4)
-
-;; b is an upperbound of f(w)
-
-(define (ub w b f geq)
-  (forall (list w) (geq b (f w))))
-
-;; lb is a lub of f
-
-(define (lub b w lb f geq)
-  (&& (ub w lb f geq)
-      (forall (list b)
-          (=> (ub w b f geq)
-              (geq b lb)))))
-
 (define (to-num b)
   (if b 1 0))
+
+(define (sigma f)
+  (lambda (w) (apply + (map f w))))
 
 (define (rule-R v R t j w)
   (|| (&& (v j w) (= t j))
       (&& (R (- t 1) j w)
-          (> j 0)
           (< j t)
           (> t 1))))
-
-(define (sigma f)
-  (lambda (w) (apply + (map f w))))
 
 (define (rule-S v R t js ws)
   ((sigma
@@ -36,14 +20,26 @@
           (* w
              (to-num
               (&& (rule-R v R t j w)
-                  (< t N)
+                  (<= 1 j)
+                  (<= j t))))))
+       ws)))
+   js))
+
+(define (S v R t js ws)
+  ((sigma
+    (lambda (j)
+      ((sigma
+        (lambda (w)
+          (* w
+             (to-num
+              (&& (R t j w)
                   (<= 1 j)
                   (<= j t))))))
        ws)))
    js))
 
 (define (rule-S-opt v R t js ws)
-  (+ (* (to-num (&& (> t 1) (< t N))) (rule-S v R (- t 1) js ws))
+  (+ (* (to-num (> t 1)) (S v R (- t 1) js ws))
      ((sigma
        (lambda (j)
          ((sigma
@@ -51,28 +47,111 @@
              (* w
                 (to-num
                  (&& (v j w)
-                     (< t N)
                      (= t j)
                      (<= 1 j))))))
           ws)))
       js)))
 
-(define (vt x w) (&& (= x 1) (= w 4)))
-(define (Rt x y z) (&& (= x 1) (= y 1) (= z 4)))
-
 (define-symbolic t tt j w integer?)
 (define-symbolic v (~> integer? integer? boolean?))
 (define-symbolic R (~> integer? integer? integer? boolean?))
+
+(define N 4)
 (define js (range N))
 (define ws (range N))
 
-(assert (< t N))
+(verify (assert (= (rule-S v R t js ws) (rule-S-opt v R t js ws))))
 
-(define sol (verify (assert (= (rule-S v R t js ws)
-                               (rule-S-opt v R t js ws)))))
+;; (define (rule-S-58 v R t js ws)
+;;   ((sigma
+;;     (lambda (j)
+;;       ((sigma
+;;         (lambda (w)
+;;           (* w
+;;              (to-num
+;;               (&& (|| (&& (v j w) (= t j))
+;;                       (&& (R (- t 1) j w)
+;;                           (< j t)
+;;                           (> t 1)))
+;;                   (<= 1 j)
+;;                   (<= j t))))))
+;;        ws)))
+;;    js))
 
-sol
+;; (define (rule-S-59 v R t js ws)
+;;   (+ ((sigma
+;;        (lambda (j)
+;;          ((sigma
+;;            (lambda (w)
+;;              (* w
+;;                 (to-num
+;;                  (&& (v j w)
+;;                      (= t j)
+;;                      (<= 1 j)
+;;                      (<= j t))))))
+;;           ws)))
+;;       js)
+;;      ((sigma
+;;        (lambda (j)
+;;          ((sigma
+;;            (lambda (w)
+;;              (* w
+;;                 (to-num
+;;                  (&& (R (- t 1) j w)
+;;                      (< j t)
+;;                      (> t 1)
+;;                      (<= 1 j)
+;;                      (<= j t))))))
+;;           ws)))
+;;       js)))
 
-(evaluate (rule-S v R t js ws) sol)
-(evaluate (rule-S-opt v R t js ws) sol)
-(evaluate (rule-S v R (- t 1) js ws) sol)
+;; (define (rule-S-60 v R t js ws)
+;;   (+ ((sigma
+;;        (lambda (j)
+;;          ((sigma
+;;            (lambda (w)
+;;              (* w
+;;                 (to-num
+;;                  (&& (v j w)
+;;                      (= t j)
+;;                      (<= 1 j))))))
+;;           ws)))
+;;       js)
+;;      ((sigma
+;;        (lambda (j)
+;;          ((sigma
+;;            (lambda (w)
+;;              (* w
+;;                 (to-num
+;;                  (&& (R (- t 1) j w)
+;;                      (< j t)
+;;                      (> t 1)
+;;                      (<= 1 j))))))
+;;           ws)))
+;;       js)))
+
+;; (define (rule-S-61 v R t js ws)
+;;   (+ ((sigma
+;;        (lambda (j)
+;;          ((sigma
+;;            (lambda (w)
+;;              (* w
+;;                 (to-num
+;;                  (&& (v j w)
+;;                      (= t j)
+;;                      (<= 1 j))))))
+;;           ws)))
+;;       js)
+;;      (* (to-num (> t 1))
+;;         ((sigma
+;;           (lambda (j)
+;;             ((sigma
+;;               (lambda (w)
+;;                 (* w
+;;                    (to-num
+;;                     (&& (R (- t 1) j w)
+;;                         (<= j (- t 1))
+;;                         (<= 1 j))))))
+;;              ws)))
+;;          js))
+;;      ))

f1474a28ed4b265e86e43222d0ec7ee2995162f9
Remy Wang <remy.sucre@gmail.com>
2020-11-12 22:43:11 -0800

    Very simple reflection.

--- reflect.rkt
+++ reflect.rkt
@@ -2,28 +2,19 @@
 
 (require (for-syntax syntax/parse) racket/set)
 
-;; remove var tags
-(define-for-syntax (to-rkt e)
-  (match e
-    [`(var ,v) v]
-    [`(,op ,x ...) (cons op (map to-rkt x))]
-    [n n]))
+(define env (make-hash))
 
-(define-for-syntax (fvs e)
-  (match e
-    [`(var ,v) (set v)]
-    [`(,op ,e ...) (apply set-union (map fvs e))]
-    [else (list->set (set))]))
+(define-syntax (var stx)
+  (syntax-parse stx
+    [(_ v) #'(hash-ref! env 'v (lambda () (define-symbolic* v integer?) v))]))
 
 (define-syntax (check-eq stx)
   (syntax-parse stx
-    [(_ e1 e2)
-     (define vars (set->list (set-union (fvs (syntax->datum #`e1)) (fvs (syntax->datum #`e2)))))
-     (define defs #`(define-symbolic #,@vars integer?))
-     (define lhs (datum->syntax defs (to-rkt (syntax->datum #`e1))))
-     (define rhs (datum->syntax defs (to-rkt (syntax->datum #`e2))))
-     #`(begin
-         #,defs
-         (verify (assert (= #,lhs #,rhs))))]))
+    [(_ e1 e2) #'(verify (assert (= e1 e2)))]))
 
 (check-eq (+ (var x) (var x)) (* 2 (var x)))
+
+(check-eq (+ (var x) (var y)) (+ (var y) (var x)))
+(check-eq (* (var z) (+ (var x) (var y)))
+          (+ (* (var z) (var y)) (* (var z) (var x))))
+(check-eq (+ (var x) (var x) (var x)) (* 2 (var x)))

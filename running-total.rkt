c994972a1a54f41fd8bd6ba95077b0e95d7b717b
Remy Wang <remy.sucre@gmail.com>
2020-11-16 21:30:55 -0800

    Clean up.

--- running-total.rkt
+++ running-total.rkt
@@ -1,92 +1,82 @@
 #lang rosette
+(require rosette/lib/synthax)
 
-(define (to-num b)
-  (if b 1 0))
+;; bound for model checking
+(define N 4)
+
+(define (I b) (if b 1 0))
 
 (define (s-sum f)
   (lambda (w) (apply + (map f w))))
 
+(define (vec-get v t)
+  ((s-sum
+    (lambda (w)
+      (* w
+         (I (&& (v t w)
+                (<= 1 t))))))
+   ws))
+
+;; f
 (define (rule-R v R t j w)
   (|| (&& (v j w) (= t j))
       (&& (R (- t 1) j w)
           (< j t)
           (> t 1))))
 
+;; g.f
 (define (rule-S v R t js ws)
   ((s-sum
     (lambda (j)
       ((s-sum
         (lambda (w)
           (* w
-             (to-num
-              (&& (rule-R v R t j w)
-                  (<= 1 j)
-                  (<= j t))))))
+             (I (&& (rule-R v R t j w)
+                    (<= 1 j)
+                    (<= j t))))))
        ws)))
    js))
 
+;; g
 (define (S v R t js ws)
   ((s-sum
     (lambda (j)
       ((s-sum
         (lambda (w)
           (* w
-             (to-num
-              (&& (R t j w)
-                  (<= 1 j)
-                  (<= j t))))))
+             (I (&& (R t j w)
+                    (<= 1 j)
+                    (<= j t))))))
        ws)))
    js))
 
+;; h.g (to be synthesized)
 (define (rule-S-opt v R t js ws)
-  (+ (S v R (- t 1) js ws)
-     ((s-sum
-       (lambda (j)
-         ((s-sum
-           (lambda (w)
-             (* w
-                (to-num
-                 (&& (v j w)
-                     (= t j)
-                     (<= 1 j))))))
-          ws)))
-      js)))
-
-(define (rule-S-out v R t js ws)
   (+ (S v R (- t 1) js ws)
      (vec-get v t)))
 
-(define (vec-get v t)
-  ((s-sum
-    (lambda (w)
-      (* w
-         (to-num
-          (&& (v t w)
-              (<= 1 t))))))
-   ws))
-
-(define-symbolic t tt j w integer?)
+(define-symbolic t integer?)
 (define-symbolic v (~> integer? integer? boolean?))
 (define-symbolic R (~> integer? integer? integer? boolean?))
 
-(define N 4)
 (define js (range N))
 (define ws (range N))
 
 (assert (<= 0 t))
 (assert (< t N))
 
-(verify (assert (= (rule-S v R t js ws) (rule-S-out v R t js ws))))
+;; g.f = h.g
+(verify (assert (= (rule-S v R t js ws) (rule-S-opt v R t js ws))))
 
-(require rosette/lib/synthax)
-
+;; grammar of semirings, with the hint to include S[t-1] in a terminal
 (define-synthax (semiring v R t js ws depth)
-  #:base (choose (S v R (- t 1) js ws) v R js ws t)
-  #:else (choose (S v R (- t 1) js ws) v R js ws t
-                 (+ (semiring v R t js ws (- depth 1))
-                    (semiring v R t js ws (- depth 1)))
-                 (vec-get (semiring v R t js ws (- depth 1))
-                          (semiring v R t js ws (- depth 1)))))
+  #:base (choose (S v R (- t 1) js ws)
+                 v R js ws t 1)
+  #:else (choose (S v R (- t 1) js ws)
+                 v R js ws t 1
+                 ((choose + - vec-get)
+                  (semiring v R t js ws (- depth 1))
+                  (semiring v R t js ws (- depth 1)))))
 
 (define (optimized v R t js ws)
   (semiring v R t js ws 3))
@@ -97,97 +87,3 @@
    #:guarantee (assert (= (optimized v R t js ws) (rule-S v R t js ws)))))
 
 (print-forms OPT)
-
-;; (define (rule-S-58 v R t js ws)
-;;   ((s-sum
-;;     (lambda (j)
-;;       ((s-sum
-;;         (lambda (w)
-;;           (* w
-;;              (to-num
-;;               (&& (|| (&& (v j w) (= t j))
-;;                       (&& (R (- t 1) j w)
-;;                           (< j t)
-;;                           (> t 1)))
-;;                   (<= 1 j)
-;;                   (<= j t))))))
-;;        ws)))
-;;    js))
-
-;; (define (rule-S-59 v R t js ws)
-;;   (+ ((s-sum
-;;        (lambda (j)
-;;          ((s-sum
-;;            (lambda (w)
-;;              (* w
-;;                 (to-num
-;;                  (&& (v j w)
-;;                      (= t j)
-;;                      (<= 1 j)
-;;                      (<= j t))))))
-;;           ws)))
-;;       js)
-;;      ((s-sum
-;;        (lambda (j)
-;;          ((s-sum
-;;            (lambda (w)
-;;              (* w
-;;                 (to-num
-;;                  (&& (R (- t 1) j w)
-;;                      (< j t)
-;;                      (> t 1)
-;;                      (<= 1 j)
-;;                      (<= j t))))))
-;;           ws)))
-;;       js)))
-
-;; (define (rule-S-60 v R t js ws)
-;;   (+ ((s-sum
-;;        (lambda (j)
-;;          ((s-sum
-;;            (lambda (w)
-;;              (* w
-;;                 (to-num
-;;                  (&& (v j w)
-;;                      (= t j)
-;;                      (<= 1 j))))))
-;;           ws)))
-;;       js)
-;;      ((s-sum
-;;        (lambda (j)
-;;          ((s-sum
-;;            (lambda (w)
-;;              (* w
-;;                 (to-num
-;;                  (&& (R (- t 1) j w)
-;;                      (< j t)
-;;                      (> t 1)
-;;                      (<= 1 j))))))
-;;           ws)))
-;;       js)))
-
-;; (define (rule-S-61 v R t js ws)
-;;   (+ ((s-sum
-;;        (lambda (j)
-;;          ((s-sum
-;;            (lambda (w)
-;;              (* w
-;;                 (to-num
-;;                  (&& (v j w)
-;;                      (= t j)
-;;                      (<= 1 j))))))
-;;           ws)))
-;;       js)
-;;      (* (to-num (> t 1))
-;;         ((s-sum
-;;           (lambda (j)
-;;             ((s-sum
-;;               (lambda (w)
-;;                 (* w
-;;                    (to-num
-;;                     (&& (R (- t 1) j w)
-;;                         (<= j (- t 1))
-;;                         (<= 1 j))))))
-;;              ws)))
-;;          js))
-;;      ))

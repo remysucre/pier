2e6dd1c6a0a9d008dc09502c3f045cdd46346ddf
Remy Wang <remy.sucre@gmail.com>
2020-11-24 15:03:35 -0800

    Cannot synthesize.

--- normalize.rkt
+++ normalize.rkt
@@ -56,6 +56,8 @@
 (struct plus (left right) #:transparent)
 (struct mul (left right) #:transparent)
 (struct agg (left right) #:transparent)
+(struct v-get (left right) #:transparent)
+(struct rel-S (t) #:transparent)
 
 (define (interpret p)
   (destruct p
@@ -63,6 +65,8 @@
     [(mul a b)  (* (interpret a) (interpret b))]
     [(sub a b)  (- (interpret a) (interpret b))]
     [(agg i e)  (sigma (interpret i) (interpret e))]
+    [(v-get a b)  (vec-get (interpret a) (interpret b))]
+    [(rel-S t)  (S (interpret t))]
     [_ p]))
 
 (define (normalize p)
@@ -82,3 +86,49 @@
 
 (verify (assert (= (interpret (normalize (rule-S-opt t)))
                    (interpret (normalize (rule-S t))))))
+
+;; ;; Grammar of semirings
+
+;; ;; op := + | -
+;; (define (??op) (choose* plus sub))
+
+;; ;; vec := v
+;; (define (??vec) (choose* v))
+
+;; ;; var := t
+;; (define (??var) (choose* t))
+
+;; ;; atom := var | number
+;; (define (??atom) (choose* (??var) (??)))
+
+;; ;; expr := atom | (op expr expr)
+;; (define (??expr depth)
+;;   (if (= depth 0)
+;;       (choose* (??atom))
+;;       (choose* (??atom)
+;;                ((??op) (??expr (- depth 1))
+;;                        (??expr (- depth 1))))))
+
+;; ;; term := (v-get v expr) | (op term term) | (S expr)
+;; (define (??term depth)
+;;   (if (= depth 1)
+;;       (choose* (v-get v (??expr (- depth 1)))
+;;                (rel-S (??expr (- depth 1))))
+;;       (choose* (v-get v (??expr (- depth 1)))
+;;                (rel-S (??expr (- depth 1)))
+;;                ((??op) (??term (- depth 1))
+;;                        (??term (- depth 1))))))
+
+;; ;; (define sketch (??term 3))
+;; (define sketch (plus (S ((??op) t 1))
+;;                      (vec-get v t)))
+
+;; (define M
+;;   (synthesize
+;;    #:forall (list v R t j w sigma)
+;;    #:guarantee (assert (= (interpret (normalize sketch))
+;;                           (interpret (normalize (rule-S t)))))))
+
+;; (evaluate sketch M)
+
+;; M

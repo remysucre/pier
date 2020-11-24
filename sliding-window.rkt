e3d13f2f2717da91c5c485ecad9f5ac1aa2d0769
Remy Wang <remy.sucre@gmail.com>
2020-11-23 18:59:23 -0800

    verify sliding window.

--- sliding-window.rkt
+++ sliding-window.rkt
@@ -1,40 +1,78 @@
 #lang rosette
 
-(define (I b)
-  (if b 1 0))
+(require rosette/lib/synthax)
 
-(define-symbolic j w t k integer?)
-(define-symbolic v (~> integer? integer? integer?))
-(define-symbolic R (~> integer? integer? integer? integer?))
+;; Bound for verification
+(define N 4)
+(define ws (range N))
 
-(verify (assert (= (* (* (v j w) (I (= t j)))
-                      (* w (* (I (>= j 1))
-                              (I (<= j t)))))
-                   (* (* (v j w) (I (= t j)))
-                      (* w (I (>= j 1)))))))
+(define (s-sum f) (apply + (map f ws)))
 
-(verify (assert (= (* (* (v j w) (I (= (- t k) j)))
-                      (* w (* (I (>= j 1))
-                              (I (<= j (- t k))))))
-                   (* (* (v j w) (I (= (- t k) j)))
-                      (* w (I (>= j 1)))))))
+(define (I b) (if b 1 0))
 
-(verify (assert (= (* (* (R (- t 1) j w)
-                         (* (I (> t 1))
-                            (I (< j t))))
-                      (* w (* (I (>= j 1)) (I (<= j t)))))
-                   (* (* (R (- t 1) j w)
-                         (* (I (> t 1))
-                            (I (< j t))))
-                      (* w (I (>= j 1)))))))
+;; v[t] = SUM_w v(t,w) * w * 1_t>=1
+(define (vec-get v t)
+  (s-sum
+    (lambda (w)
+      (* w
+         (I (&& (v t w)
+                (<= 1 t)))))))
 
-(verify (assert (= (* (* (R (- (- t k) 1) j w)
-                         (I (> k 0))
-                         (* (I (> (- t k) 1))
-                            (I (< j (- t k)))))
-                      (* w (* (I (>= j 1)) (I (<= j (- t k))))))
-                   (* (* (R (- (- t k) 1) j w) w)
-                      (I (> k 0))
-                      (* (* (I (<= j (- (- t k) 1)))
-                            (I (<= 1 j)))
-                         (I (> t 1)))))))
+(define-symbolic v (~> integer? integer? boolean?))
+(define-symbolic R (~> integer? integer? integer? boolean?))
+
+;; f(R)
+(define (rule-R t j w)
+  (|| (&& (v j w) (= t j))
+      (&& (R (- t 1) j w)
+          (< j t)
+          (> t 1))))
+
+(define (rule-prefix t)
+  (s-sum
+    (lambda (j)
+      (s-sum
+        (lambda (w)
+          (* w
+             (I (&& (rule-R t j w)
+                    (<= 1 j)
+                    (<= j t)))))))))
+
+(define (prefix t)
+  (s-sum
+    (lambda (j)
+      (s-sum
+        (lambda (w)
+          (* w
+             (I (&& (R t j w)
+                    (<= 1 j)
+                    (<= j t)))))))))
+
+;; g(f(R))
+(define (rule-S t k)
+  (- (rule-prefix t) (rule-prefix (- t k)) #;(* (rule-prefix (- t k)) (I (> k 0)))))
+
+
+;; g(R)
+(define (S t k)
+  (- (prefix t) (prefix (- t k)) #;(* (prefix (- t k)) (I (> k 0)))))
+
+;; h(g(R)) (h to be synthesized)
+(define (rule-S-opt t k)
+  (+ (S (- t 1) k)
+     (- (vec-get v t)
+        (vec-get v (- t k)))))
+
+(define-symbolic t k integer?)
+
+;; Necessary for bounded verification
+(assert (<= 0 t))
+(assert (< t N))
+
+(assert (> k 0))
+
+(define sol (verify (assert (= (rule-S t k) (rule-S-opt t k)))))
+
+;; (evaluate (rule-S t k) sol)
+;; (evaluate (rule-S-opt t k) sol)
+sol

d2729d117b5dea6930d486227598ecc2e5ee9450
Remy Wang <remy.sucre@gmail.com>
2020-12-09 10:38:43 -0800

    Clean up.

--- apsp/apsp-union.rkt
+++ apsp/apsp-union.rkt
@@ -8,42 +8,6 @@
          "defs.rkt"
          "interp.rkt")
 
-;; (29) ... + MIN_{y,w1,w2,w} w * R(x,y,w1) * E(y,x,w2) * 1_{w=w1*w2}
-(define l
-  (interpret
-   (push-sum
-    (simplify
-     (push-sum
-      (op-t+ (op-weight 'w 'x 'z)
-      (op-sum-int
-       'y
-       (op-sum
-        'w1
-        (op-sum
-         'w2
-         (op-sum
-          'w
-          (op-t* 'w
-                 (op-t* (op-I (rel-R 'x 'y 'w1))
-                        (op-t* (op-I (rel-E 'y 'x 'w2))
-                               (op-I (op-eq? 'w (op-t* 'w1 'w2))))))))))))))))
-
-;; (31) ... + MIN_{y,w1,w2} R(x,y,w1) * E(y,x,w2) * w1 * w2
-(define r
-  (interpret
-   (push-sum
-    (op-t+ (op-weight 'w 'x 'z)
-      (op-sum-int
-       'y
-       (op-sum
-        'w2
-        (op-sum
-         'w1
-         (op-t* (op-t* (op-I (rel-R 'x 'y 'w1)) 'w1)
-                (op-t* (op-I (rel-E 'y 'x 'w2)) 'w2)))))))))
-
-(verify (assert (eq? l r)))
-
 (define (??v) (choose* 'x 'y 'z))
 (define (??w) (choose* 'w 'w1 'w2))
 (define (??op) (choose* op-t+ op-t*))

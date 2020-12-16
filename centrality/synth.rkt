d3301ec84a46a694f4eae6d86f5cdee6bb482c4c
Remy Wang <remy.sucre@gmail.com>
2020-12-16 13:17:41 -0800

    Fix division.

--- centrality/synth.rkt
+++ centrality/synth.rkt
@@ -9,15 +9,18 @@
          "interp.rkt")
 
 (define S-237
-  (op-+ (op-sum 't (op-delta 's 'v 't))
+  (op-+ (op-sum 't (op-* (op-I (rel-E 'v 't))
+                         (op-delta 's 'v 't)))
         (op-sum 'u
                 (op-sum 't
-                        (op-* (op-delta 's 'v 't)
-                              (op-delta 's 't 'u))))))
+                        (op-* (op-I (rel-E 'v 't))
+                              (op-* (op-delta 's 'v 't)
+                                    (op-delta 's 't 'u)))))))
 
 (define S-238
   (op-+ (op-sum 't (op-* (op-I (rel-E 'v 't))
-                         (op-delta 's 'v 't)))
+                         (op-* (op-sigma 's 'v 't)
+                               (op-inv (rel-sigma 's 't)))))
         (op-sum 'u
                 (op-sum 't
                         (op-/ (op-* (op-* (op-I (rel-E 'v 't))
@@ -34,21 +37,20 @@
 
 (define-symbolic x y z integer?)
 (assert (forall (list x y z) (<= (D x z) (+ (D x y) (D y z)))))
-(assert (forall (list x) (= (* x (inv x)) 1)))
+(assert (= (* (inv (sigma s u)) (sigma s u)) 1))
+(assert (= (* (inv (sigma s t)) (sigma s t)) 1))
 
-;; x * a / x * b = a / b
-;; x * a * inv(x * b)
+;; NOTE the following is unsat
+;; (assert (forall (list x) (=> (> x 0) (= (* x (inv x)) 1))))
 
-;; inv(x * y) = inv(x) * inv(y)
-
-;; x * inv(x) = 1
-
+;; NOTE try the following when z3 returns unknown
 ;; (assert (<= (D s u) (+ (D s t) (D t u))))
 ;; (assert (<= (D s t) (+ (D s v) (D v t))))
 ;; (assert (<= (D s u) (+ (D s v) (D v u))))
 ;; (assert (<= (D v u) (+ (D v t) (D t u))))
 
-(verify (assert (eq? (interpret S-237) (interpret S-238))))
+;; (verify (assert (not (= (interpret S-237) (interpret S-238)))))
+(verify (assert (= (interpret S-237) (interpret S-238))))
 
 ;; (define (??v) (choose* 'x 'y 'z (??)))
 ;; (define (??op) (choose* op-+ op-*))

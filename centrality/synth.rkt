8fb397a8e300d7c8abb23567e214b324230ecd6b
Remy Wang <remy.sucre@gmail.com>
2020-12-16 13:26:21 -0800

    Synthesize centrality.

--- centrality/synth.rkt
+++ centrality/synth.rkt
@@ -9,8 +9,9 @@
          "interp.rkt")
 
 (define S-237
-  (op-+ (op-sum 't (op-* (op-I (rel-E 'v 't))
-                         (op-delta 's 'v 't)))
+  (op-+ (op-sum 't
+                (op-* (op-I (rel-E 'v 't))
+                      (op-delta 's 'v 't)))
         (op-sum 'u
                 (op-sum 't
                         (op-* (op-I (rel-E 'v 't))
@@ -52,38 +53,28 @@
 ;; (verify (assert (not (= (interpret S-237) (interpret S-238)))))
 (verify (assert (= (interpret S-237) (interpret S-238))))
 
-;; (define (??v) (choose* 'x 'y 'z (??)))
-;; (define (??op) (choose* op-+ op-*))
+(define (??v) (choose* 's 'u 'v 't))
+(define (??op) (choose* op-+ op-*))
 
-;; (define (??term depth)
-;;   (if (= 0 depth)
-;;       (choose* (??v)
-;;                (rel-R (??v) (??v))
-;;                (rel-E (??v) (??v)))
-;;       (choose* ((??op) (??term (- depth 1)) (??term (- depth 1)))
-;;                (op-exists (??v) (??term (- depth 1))))))
+(define (??term depth)
+  (if (= 0 depth)
+      (choose* (??v)
+               (op-I (rel-E (??v) (??v)))
+               (op-delta (??v) (??v) (??v)))
+      ((??op) (??term (- depth 1)) (??term (- depth 1)))))
 
-;; (define S-29
-;;   (op-+ (rel-E 1 'y)
-;;         (op-exists 'z
-;;                    (op-* (rel-R 1 'z)
-;;                          (rel-E 'z 'y)))))
+(define sketch
+  (op-+ (op-sum 't
+                (op-* (op-I (rel-E 'v 't))
+                      (op-delta 's 'v 't)))
+        (op-sum (??v)
+                (op-sum 't
+                        (op-* (op-delta 's 'v 't)
+                              (??term 1))))))
 
-;; ??term +
-;; MIN_{?v ...}
-;;   MIN_w1
-;;     MIN_{?u ...} R(x, y, w1) * w1
-;;                 * ??term
+(define M
+  (synthesize
+   #:forall (list E sigma D s t u v sig inv)
+   #:guarantee (assert (eq? (interpret sketch) (interpret S-238)))))
 
-;; (define sketch
-;;   (op-+ (??term 0)
-;;         (op-exists (??v)
-;;                    (op-* (rel-R 1 (??v))
-;;                          (??term 0)))))
-
-;; (define M
-;;   (synthesize
-;;    #:forall (list E R x y z sig)
-;;    #:guarantee (assert (eq? (interpret sketch) (interpret S-29)))))
-
-;; (evaluate sketch M)
+(evaluate sketch M)

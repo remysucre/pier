c53f5f81557732650763b7d8e417afe6f506db86
Remy Wang <remy.sucre@gmail.com>
2021-01-04 14:27:05 -0800

    Remove old code.

--- old/play/play.rkt
+++
@@ -1,9 +0,0 @@
-#lang rosette/safe
-
-(require rosette/lib/angelic
-         rosette/lib/destruct)
-
-(define (w) (define-symbolic x real?) x)
-(define (u) (define-symbolic x real?) x)
-
-(verify (assert (eq? (w) (w))))

8d6d04a871c36ec381826bf0d7d9e34b21d226f8
Remy Wang <remy.sucre@gmail.com>
2021-02-18 17:13:26 -0800

    Clean up process.

--- core/process.rkt
+++ core/process.rkt
@@ -13,25 +13,3 @@
     [`(+ ,x ,y) (op-+ (preprocess get-def x) (preprocess get-def y))]
     [`(sum ,y ,e) (op-sum y (preprocess get-def e))]
     [`(,f ,vs ...) (apply (get-def f) (map (curry preprocess get-def) vs))]))
-
-(define (pr p var rel)
-  (define (proc p)
-    (match p
-      [`(sum ,v ,e) (op-sum (proc v) (proc e))]
-      [`(* ,x ,y) (op-* (proc x) (proc y))]
-      [`(I ,e) (op-I (proc e))]
-      [`(,r ,xs ...) (op-rel (hash-ref rel r) (map proc xs))]
-      [x (hash-ref var x)]))
-  (proc p))
-
-(define (show p ler nuf rav)
-  (define (show p)
-  (match p
-    [(op-+ x y) `(+ ,(show x) ,(show y))]
-    [(op f xs) (cons (hash-ref nuf f) (map show xs))]
-    [(op-sum v e) `(sum ,(hash-ref rav v) ,(show e))]
-    [(op-* x y) `(* ,(show x) ,(show y))]
-    [(op-I e) `(I ,(show e))]
-    [(op-rel r xs) (cons (hash-ref ler r) (map show xs))]
-    [x (hash-ref rav x)]))
-  (show p))

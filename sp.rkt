26bbf3034cb4937a1977bbc06395294ec4130927
Remy Wang <remy.sucre@gmail.com>
2021-04-01 15:10:26 -0700

    Format.

--- sp.rkt
+++ sp.rkt
@@ -8,21 +8,21 @@
 
 ;; define helper functions
 (def (weight w x z)
-  ;; MIN_w . 1_{E(x,z,w)} + w.
+  ;; min w . 1_{E(x,z,w)} + w.
   (sum w (* (I (E x z w)) w)))
 
 ;; recursive stratum
 (rec (f R)
-     (λ (x z w) (+
-        ;; R(x,z,w) :- E(x,z,w).
-        (I (E x z w))
-        ;; R(x,z,w) :- R(x,y,w1), E(y,x,w2), w=w1+w2.
-        (sum y (sum w1 (sum w2
-           (* (* (I (R x y w1)) (I (E y z w2))) (I (= w (* w1 w2))))))))))
+  (λ (x z w) (+
+     ;; R(x,z,w) :- E(x,z,w).
+     (I (E x z w))
+     ;; R(x,z,w) :- R(x,y,w1), E(y,x,w2), w=w1+w2.
+     (sum y (sum w1 (sum w2
+       (* (* (I (R x y w1)) (I (E y z w2))) (I (= w (* w1 w2))))))))))
 
 ;; "return" stratum
 (ret (g R)
-     ;; S[x,z] = MIN_w . R(x,z,w) + w.
-     (λ (x z) (sum w (* (R x z w) w))))
+  ;; S[x,z] = min w . R(x,z,w) + w.
+  (λ (x z) (sum w (* (R x z w) w))))
 
 (optimize)

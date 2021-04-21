be59e867e3c5f830c55779acfd9c30ca82902368
Remy Wang <remy.sucre@gmail.com>
2021-04-20 17:19:27 -0700

    actually make sw work.

--- sw.rkt
+++ sw.rkt
@@ -42,7 +42,7 @@
 (hash-update! type->var 'id? (curry cons (op-- t k)))
 ;; (hash-update! type->var 'int? (curry cons 0))
 
-;; (optimize)
+(optimize)
 
 ;; (+ (- (vec-get j w t) (vec-get j w (- t k))) (S (- t 1) k))
 #;(- (+ (sum j (sum w (* (I (<= j t)) (* (I (<= 1 j)) (* (I (= j t)) (* w (I (rel v j w))))))))
@@ -97,51 +97,51 @@
                                            (op-* w
                                                  (op-I (op-leq j (op-+ (op-+ t (op-* -1 k)) -1))))))))))))))
 
-(define ??t-1 (choose* t j k (op-- t 1) (op-- t k)))
-(define ??j (choose* t j k (op-- t 1) (op-- t k)))
-(define ??term-0 (choose* w 0 1 -1))
+;; (define ??t-1 (choose* t j k (op-- t 1) (op-- t k)))
+;; (define ??j (choose* t j k (op-- t 1) (op-- t k)))
+;; (define ??term-0 (choose* w 0 1 -1))
 
-(define sketch
-  (op-+
-   (op-+
-    (op-sum
-     j (op-sum
-        w (op-* (op-I (op-leq j t))
-                (op-* (op-I (op-leq 1 j))
-                      (op-* (op-I (op-eq? j t))
-                            (op-* w
-                                  (op-I (op-rel v (list j w)))))))))
-    (op-sum
-     j (op-sum
-        w (op-* ??term-0
-                (op-* w
-                      (op-* (op-I (op-leq 1 ??j))
-                            (op-* (op-I (op-rel R (list ??t-1 ??j w)))
-                                  (op-I (op-leq ??j ??t-1)))))))))
-   (op-+
-    (op-sum
-     j (op-sum
-        w (op-* -1
-                (op-* (op-I (op-leq 1 j))
-                      (op-* (op-I (op-leq j (op-+ t (op-* -1 k))))
-                            (op-* (op-I (op-eq? j (op-+ t (op-* -1 k))))
-                                  (op-* w (op-I (op-rel v (list j w))))))))))
-    (op-sum
-     j (op-sum
-        w (op-* -1 (op-* ??term-0
-                         (op-* (op-I (op-leq 1 ??j))
-                               (op-* (op-I (op-rel R (list (op-+ ??t-1 (op-* -1 k)) ??j w)))
-                                     (op-* w
-                                           (op-I (op-leq ??j (op-+ ??t-1 (op-* -1 k))))))))))))))
+;; (define sketch
+;;   (op-+
+;;    (op-+
+;;     (op-sum
+;;      j (op-sum
+;;         w (op-* (op-I (op-leq j t))
+;;                 (op-* (op-I (op-leq 1 j))
+;;                       (op-* (op-I (op-eq? j t))
+;;                             (op-* w
+;;                                   (op-I (op-rel v (list j w)))))))))
+;;     (op-sum
+;;      j (op-sum
+;;         w (op-* ??term-0
+;;                 (op-* w
+;;                       (op-* (op-I (op-leq 1 ??j))
+;;                             (op-* (op-I (op-rel R (list ??t-1 ??j w)))
+;;                                   (op-I (op-leq ??j ??t-1)))))))))
+;;    (op-+
+;;     (op-sum
+;;      j (op-sum
+;;         w (op-* -1
+;;                 (op-* (op-I (op-leq 1 j))
+;;                       (op-* (op-I (op-leq j (op-+ t (op-* -1 k))))
+;;                             (op-* (op-I (op-eq? j (op-+ t (op-* -1 k))))
+;;                                   (op-* w (op-I (op-rel v (list j w))))))))))
+;;     (op-sum
+;;      j (op-sum
+;;         w (op-* -1 (op-* ??term-0
+;;                          (op-* (op-I (op-leq 1 ??j))
+;;                                (op-* (op-I (op-rel R (list (op-+ ??t-1 (op-* -1 k)) ??j w)))
+;;                                      (op-* w
+;;                                            (op-I (op-leq ??j (op-+ ??t-1 (op-* -1 k))))))))))))))
 
-;; (verify (assert (= (interpret sketch) (interpret (exp->struct (normalize ((g (f r)) 't 'k)) symbol->var symbol->rel symbol->fun)))))
-(define g-f-r (exp->struct (normalize ((g (f r)) 't 'k)) symbol->var symbol->rel symbol->fun))
+;; ;; (verify (assert (= (interpret sketch) (interpret (exp->struct (normalize ((g (f r)) 't 'k)) symbol->var symbol->rel symbol->fun)))))
+;; (define g-f-r (exp->struct (normalize ((g (f r)) 't 'k)) symbol->var symbol->rel symbol->fun))
 
-(define M
-  (synthesize
-   #:forall (append (hash-values symbol->rel)
-                    (hash-values symbol->var)
-                    #;(hash-values symbol->fun)
-                    (list sum inv))
-   #:guarantee (assert (eq? (interpret sketch) (interpret g-f-r)))))
-(evaluate sketch M)
+;; (define M
+;;   (synthesize
+;;    #:forall (append (hash-values symbol->rel)
+;;                     (hash-values symbol->var)
+;;                     #;(hash-values symbol->fun)
+;;                     (list sum inv))
+;;    #:guarantee (assert (eq? (interpret sketch) (interpret g-f-r)))))
+;; (evaluate sketch M)
